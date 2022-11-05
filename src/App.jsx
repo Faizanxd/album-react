6262e585f603b4e9bec29c3b4e35bff3292dbbe3
Faizan <fs8167@gmail.com>
2022-11-05 21:37:18 +0530

    scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react'
 import Content from './Content'
-import Navbar from './Navbar';
+import Navbar from './Navbar'
 import './index.css'
+// import { browerRouter as Router, Route, Switch } from 'react-router-dom'
+
+const api = "https://rickandmortyapi.com/api/character"
+
 
 
 const App =()=> {
   const[Characters, setCharacters] = useState(null);
-
+  const [url, setUrl] = useState(api)
+  
  
   useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character')
+    fetch(url)
       .then(res => {
         return res.json();
       })
@@ -20,9 +25,17 @@ const App =()=> {
 
  return (
     <div className='font-body'>
+    {/* <Router>  */}
         <Navbar/>
-       {Characters && <Content Characters={Characters} />}
+        {Characters && <Content Characters={Characters} setUrl={setUrl}/>}
+          {/* <Switch>
+            <Route path='/'>
+           <Content Characters={Characters} />
+       </Route>
+          </Switch>
+      </Router> */}
    </div>
+  
     )
-  }
+}
 export default App
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,58 @@
 const Content =({ Characters })=>{
-return(
+        
+        return(
         <>
 <div className="">
+           <div className="flex justify-end">
+                <button className="rounded-lg btn-2 m-2" id="previous-btn" onClick={handlePreviousClick}>Previous</button>
+                <button className="rounded-lg btn-2 m-2 " id="next-btn" onClick={handleNextClick}>Next</button>
+        </div>
         <div className="grid lg:grid-cols-4 place-items-center grid-cols-3">
        {Characters.results.map(item=>(
         <div className="border-2 card" key={item.id}>
                 <img src={item.image} alt="" className=""/>
                 <h3 className="font-bold text-2xl text-center">{item.name}</h3>
-                <h2 className="text-center text-lime-800 font-bold text-xl">{item.status}</h2>
+                {
+                        status({Characters:item})
+        
+                }
+        
                 <div className="flex justify-center">
                   <button className="text-center w-[40%] btn">More </button>
                 </div>
         </div>
         ))}
+        
         </div>
 </div>
 </>
 
 )}
+
+const handleNextClick = ({Characters,setUrl}) => {
+     setUrl(Characters.next)
+}
+
+const handlePreviousClick = ({Characters,setUrl}) => {
+        setUrl(Characters.prev)
+}
+
+const status =({ Characters })=>{
+        if(Characters.status === "Alive"){
+                return(
+                        <h2 className="text-center text-lime-800 font-bold text-2xl">{Characters.status}</h2>
+                )
+        }
+        else if(Characters.status === "Dead"){
+                return(
+                        <h2 className="text-center text-red-800 font-bold text-2xl">{Characters.status}</h2>
+                )
+        }
+        else{
+                return(
+                        <h2 className="text-center text-yellow-800 font-bold text-2xl">{Characters.status}</h2>
+                )
+        }
+}
 
 export default Content
